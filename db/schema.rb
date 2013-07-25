b0fdacbf4e9dc0ba3833097908765cdd0a3be9d5
JulianArnold <julian_arnold@hotmail.com>
2013-07-25 14:21:27 +0100

    development.rb file correction

--- db/schema.rb
+++ db/schema.rb
@@ -13,39 +13,39 @@
 
 ActiveRecord::Schema.define(:version => 20130521194140) do
 
-  create_table "carts", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "carts",           :force => true do |t|
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
-  create_table "line_items", :force => true do |t|
+  create_table "line_items",      :force => true do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-    t.integer  "quantity",   :default => 1
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.integer  "quantity",        :default => 1
     t.integer  "order_id"
   end
 
-  create_table "orders", :force => true do |t|
+  create_table "orders",          :force => true do |t|
     t.string   "name"
     t.text     "address"
     t.string   "email"
     t.string   "pay_type"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
-  create_table "products", :force => true do |t|
+  create_table "products",        :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users",           :force => true do |t|
     t.string   "name"
     t.string   "password_digest"
     t.datetime "created_at",      :null => false

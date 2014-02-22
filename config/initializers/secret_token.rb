819bf662317af86fc345fd89d4f09cb0b81863c0
JulianArnold <julian_arnold@hotmail.com>
2014-02-22 20:16:11 +0000

    sevret key code changed

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Grushenka::Application.config.secret_token = 'a59fa1c57d680a7ffcf9c4ac7020bf5084df3846e1c38d8e5544bcdd71faca24e349b59a48d3170639111431f5f9c1371cd50bad8f3f4a7848e6008ddd825929'
+Grushenka::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+                                               ('x' * 30) # meets minimum requirement of 30 chars long
+                                             else
+                                               ENV['SECRET_TOKEN']
+                                             end
